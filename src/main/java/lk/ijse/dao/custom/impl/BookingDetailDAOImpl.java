e5ea1fea8656eb1af6cd271cd47afdde9630be9b
UchithmaSenevirathne <uchithma@gmail.com>
2023-12-22 06:53:49 +0530

    content update

--- src/main/java/lk/ijse/dao/custom/impl/BookingDetailDAOImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/BookingDetailDAOImpl.java
@@ -1,5 +1,6 @@
 package lk.ijse.dao.custom.impl;
 
+import lk.ijse.dao.SQLUtil;
 import lk.ijse.dao.custom.BookingDetailDAO;
 import lk.ijse.db.DbConnection;
 import lk.ijse.dto.BookingDetailDTO;
@@ -12,59 +13,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BookingDetailDAOImpl implements BookingDetailDAO {
-    /*public static BookingDetailDTO searchbookingDetail(String bId) throws SQLException {
-        Connection connection = DbConnection.getInstance().getConnection();
-
-        String sql = "SELECT * FROM bookingdetail WHERE bId = ?";
-
-        PreparedStatement pstm = connection.prepareStatement(sql);
-
-        pstm.setString(1, bId);
-
-        ResultSet resultSet = pstm.executeQuery();
-
-        BookingDetailDTO dto = null;
-
-        if(resultSet.next()) {
-            String b_Id = resultSet.getString(1);
-            String car_Id = resultSet.getString(2);
-            String dr_Id = resultSet.getString(3);
-
-            dto = new BookingDetailDTO(b_Id,car_Id,dr_Id);
-        }
-
-        return dto;
-    }*/
-    /*public boolean saveBookingDetail(String bId, List<BookTm> bookList) throws SQLException {
-        for (BookTm bookTm : bookList) {
-            if(!saveBookingDetail(bId, bookTm)) {
-                return false;
-            }
-        }
-        return true;
-    }*/
     @Override
     public boolean save(BookingDetailDTO bookingDetail) throws SQLException {
-        Connection connection = DbConnection.getInstance().getConnection();
-
-        String sql = "INSERT INTO bookingDetail VALUES(?, ?, ?)";
-        PreparedStatement pstm = connection.prepareStatement(sql);
-        pstm.setString(1, bookingDetail.getBId());
-        pstm.setString(2, bookingDetail.getCarNo());
-        pstm.setString(3, bookingDetail.getDriverId());
-
-        return pstm.executeUpdate() > 0;
+        return SQLUtil.execute("INSERT INTO bookingDetail VALUES(?, ?, ?)",
+                bookingDetail.getBId(),
+                bookingDetail.getCarNo(),
+                bookingDetail.getDriverId()
+        );
     }
     @Override
     public List<BookingDetailDTO> getAll() throws SQLException {
-        Connection connection = DbConnection.getInstance().getConnection();
-
-        String sql = "SELECT * FROM bookingdetail";
-        PreparedStatement pstm = connection.prepareStatement(sql);
-
         List<BookingDetailDTO> dtoList = new ArrayList<>();
 
-        ResultSet resultSet = pstm.executeQuery();
+        ResultSet resultSet = SQLUtil.execute("SELECT * FROM bookingdetail");
 
         while (resultSet.next()){
             String book_Id = resultSet.getString(1);
@@ -89,13 +50,9 @@ public class BookingDetailDAOImpl implements BookingDetailDAO {
 
     @Override
     public boolean delete(String bId) throws SQLException {
-        Connection connection = DbConnection.getInstance().getConnection();
-
-        PreparedStatement pstm1 = connection.prepareStatement("DELETE FROM bookingdetail WHERE bId = ?");
-
-        pstm1.setString(1, bId);
-
-        return pstm1.executeUpdate() > 0;
+        return SQLUtil.execute("DELETE FROM bookingdetail WHERE bId = ?",
+                bId
+        );
     }
 
     @Override
